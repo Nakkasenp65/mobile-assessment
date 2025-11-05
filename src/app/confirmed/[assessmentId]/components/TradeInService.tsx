055bd057bfaeb9eaf6203b9708e84dd8244da619
Nakkasenp65 <nakkasenputtacharoen@gmail.com>
2025-11-05 15:31:34 +0700

    refactor: Remove nextSteps rendering from PawnService and TradeInService components

--- src/app/confirmed/[assessmentId]/components/TradeInService.tsx
+++ src/app/confirmed/[assessmentId]/components/TradeInService.tsx
@@ -65,17 +65,6 @@ export default function TradeInService({ assessment, info }: TradeInServiceProps
           </span>
         </div>
       </div>
-
-      {info?.nextSteps && info.nextSteps.length > 0 && (
-        <div className="mt-4 rounded-lg bg-gray-50 p-4">
-          <p className="text-sm font-semibold text-gray-900">ขั้นตอนถัดไป</p>
-          <ul className="mt-2 list-disc space-y-1 pl-4 text-sm text-gray-700">
-            {info.nextSteps.map((s, i) => (
-              <li key={i}>{s}</li>
-            ))}
-          </ul>
-        </div>
-      )}
     </section>
   );
 }
